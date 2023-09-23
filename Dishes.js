15fd09d5f205f7bbf4ba02a47bda0c54733d9ad6
Ronak BCA <ronakknowledge57@gmail.com>
2023-09-24 02:14:36 +0530

    Day 03 : Today, I wrote a code of dining out and nightlife option of dish navigation bar.

---
+++ Dishes.js
@@ -0,0 +1,118 @@
+document.getElementsByClassName('innermain1')[1].addEventListener("click", function () {
+
+    document.getElementsByClassName('proimg')[0].src = "./Project_Img/hotel2.webp";
+    document.getElementsByClassName('proimg')[1].src = "./Project_Img/hotel1.webp";
+    document.getElementsByClassName('proimg')[2].src = "./Project_Img/hotel3.webp";
+    document.getElementsByClassName('proimg')[3].src = "./Project_Img/hotel4.webp";
+    document.getElementsByClassName('proimg')[4].src = "./Project_Img/hotel5.webp";
+    document.getElementsByClassName('proimg')[5].src = "./Project_Img/hotel6.webp";
+    document.getElementsByClassName('proimg')[6].src = "./Project_Img/hotel7.webp";
+    document.getElementsByClassName('proimg')[7].src = "./Project_Img/hotel8.webp";
+    document.getElementsByClassName('proimg')[8].src = "./Project_Img/hotel9.webp";
+    document.getElementsByClassName('proimg')[9].src = "./Project_Img/hotel10.webp";
+    document.getElementsByClassName('proimg')[10].src = "./Project_Img/hotel11.webp";
+    document.getElementsByClassName('proimg')[11].src = "./Project_Img/hotel12.webp";
+    document.getElementsByClassName('proimg')[12].src = "./Project_Img/hotel13.webp";
+    document.getElementsByClassName('proimg')[13].src = "./Project_Img/hotel15.webp";
+    document.getElementsByClassName('proimg')[14].src = "./Project_Img/hotel14.webp";
+    document.getElementsByClassName('proimg')[15].src = "./Project_Img/hotel16.webp";
+    document.getElementsByClassName('proimg')[16].src = "./Project_Img/hotel17.webp";
+    document.getElementsByClassName('proimg')[17].src = "./Project_Img/hotel18.webp";
+
+    document.getElementsByClassName('insideoff-img')[0].src="./Project_Img/slider1.jpg";
+
+    document.getElementsByClassName('cimg')[0].src = "./Project_Img/taj.jpg";
+    document.getElementsByClassName('cimg')[1].src = "./Project_Img/taj5.jpg";
+    document.getElementsByClassName('cimg')[2].src = "./Project_Img/taj1.jpg";
+    document.getElementsByClassName('cimg')[3].src = "./Project_Img/taj2.jpg";
+    document.getElementsByClassName('cimg')[4].src = "./Project_Img/taj3.jpg";
+    document.getElementsByClassName('cimg')[5].src = "./Project_Img/taj4.jpg";
+    document.getElementsByClassName('cimg')[6].src = "./Project_Img/taj5.jpg";
+    document.getElementsByClassName('cimg')[7].src = "./Project_Img/taj6.jpg";
+    document.getElementsByClassName('cimg')[8].src = "./Project_Img/taj7.jpg";
+    document.getElementsByClassName('cimg')[9].src = "./Project_Img/taj8.jpg";
+    document.getElementsByClassName('cimg')[10].src = "./Project_Img/taj9.jpg";
+    document.getElementsByClassName('cimg')[11].src = "./Project_Img/taj10.jpg";
+
+});
+
+
+
+document.getElementsByClassName('innermain1')[0].addEventListener("click", function () {
+
+    document.getElementsByClassName('proimg')[0].src = "./Project_Img/pro2.jpg";
+    document.getElementsByClassName('proimg')[1].src = "./Project_Img/pro3.webp";
+    document.getElementsByClassName('proimg')[2].src = "./Project_Img/pro4.webp";
+    document.getElementsByClassName('proimg')[3].src = "./Project_Img/noodles1.webp";
+    document.getElementsByClassName('proimg')[4].src = "./Project_Img/noodles3.jpg";
+    document.getElementsByClassName('proimg')[5].src = "./Project_Img/noodles2.webp";
+    document.getElementsByClassName('proimg')[6].src = "../Project_Img/tr2.webp";
+    document.getElementsByClassName('proimg')[7].src = "./Project_Img/tr1.webp";
+    document.getElementsByClassName('proimg')[8].src = "./Project_Img/tr3.webp";
+    document.getElementsByClassName('proimg')[9].src = "./Project_Img/tr4.webp";
+    document.getElementsByClassName('proimg')[10].src = "./Project_Img/tr5.webp";
+    document.getElementsByClassName('proimg')[11].src = "./Project_Img/tr6.webp";
+    document.getElementsByClassName('proimg')[12].src = "./Project_Img/ch1.webp";
+    document.getElementsByClassName('proimg')[13].src = "./Project_Img/ch2.webp";
+    document.getElementsByClassName('proimg')[14].src = "./Project_Img/ch3.webp";
+    document.getElementsByClassName('proimg')[15].src = "./Project_Img/ch4.webp";
+    document.getElementsByClassName('proimg')[16].src = "./Project_Img/ch5.webp";
+    document.getElementsByClassName('proimg')[17].src = "./Project_Img/ch6.jpg";
+
+    document.getElementsByClassName('insideoff-img')[0].src="./Project_Img/off.webp";
+
+    document.getElementsByClassName('cimg')[0].src="./Project_Img/cir1.webp";
+    document.getElementsByClassName('cimg')[1].src="./Project_Img/cir2.webp";
+    document.getElementsByClassName('cimg')[2].src="./Project_Img/cir3.webp";
+    document.getElementsByClassName('cimg')[3].src="./Project_Img/cir4.webp";
+    document.getElementsByClassName('cimg')[4].src="./Project_Img/cir5.webp";
+    document.getElementsByClassName('cimg')[5].src="./Project_Img/cir6.webp";
+    document.getElementsByClassName('cimg')[6].src="./Project_Img/ch1.webp" ;
+    document.getElementsByClassName('cimg')[7].src="./Project_Img/ch7.webp" ;
+    document.getElementsByClassName('cimg')[8].src="./Project_Img/ch9.jpg" ;
+    document.getElementsByClassName('cimg')[9].src="./Project_Img/ch3.webp" ;
+    document.getElementsByClassName('cimg')[10].src="./Project_Img/ch2.webp";
+    document.getElementsByClassName('cimg')[11].src="./Project_Img/ch5.webp";
+
+    
+});
+
+
+document.getElementsByClassName('innermain1')[2].addEventListener("click", function () {
+    document.getElementsByClassName('proimg')[0].src = "./Project_Img/n1.webp";
+    document.getElementsByClassName('proimg')[1].src = "./Project_Img/n2.jpg";
+    document.getElementsByClassName('proimg')[2].src = "./Project_Img/n3.webp";
+    document.getElementsByClassName('proimg')[3].src = "./Project_Img/n4.webp";
+    document.getElementsByClassName('proimg')[4].src = "./Project_Img/n5.webp";
+    document.getElementsByClassName('proimg')[5].src = "./Project_Img/n6.webp";
+    document.getElementsByClassName('proimg')[6].src = "./Project_Img/hotel7.webp";
+    document.getElementsByClassName('proimg')[7].src = "./Project_Img/hotel8.webp";
+    document.getElementsByClassName('proimg')[8].src = "./Project_Img/hotel9.webp";
+    document.getElementsByClassName('proimg')[9].src = "./Project_Img/hotel10.webp";
+    document.getElementsByClassName('proimg')[10].src = "./Project_Img/hotel11.webp";
+    document.getElementsByClassName('proimg')[11].src = "./Project_Img/hotel12.webp";
+    document.getElementsByClassName('proimg')[12].src = "./Project_Img/hotel13.webp";
+    document.getElementsByClassName('proimg')[13].src = "./Project_Img/hotel15.webp";
+    document.getElementsByClassName('proimg')[14].src = "./Project_Img/hotel14.webp";
+    document.getElementsByClassName('proimg')[15].src = "./Project_Img/hotel16.webp";
+    document.getElementsByClassName('proimg')[16].src = "./Project_Img/hotel17.webp";
+    document.getElementsByClassName('proimg')[17].src = "./Project_Img/hotel18.webp";
+
+    document.getElementsByClassName('insideoff-img')[0].src="./Project_Img/slider2.jpg";
+
+    document.getElementsByClassName('cimg')[0].src = "./Project_Img/taj.jpg";
+    document.getElementsByClassName('cimg')[1].src = "./Project_Img/taj5.jpg";
+    document.getElementsByClassName('cimg')[2].src = "./Project_Img/taj1.jpg";
+    document.getElementsByClassName('cimg')[3].src = "./Project_Img/taj2.jpg";
+    document.getElementsByClassName('cimg')[4].src = "./Project_Img/taj3.jpg";
+    document.getElementsByClassName('cimg')[5].src = "./Project_Img/taj4.jpg";
+    document.getElementsByClassName('cimg')[6].src = "./Project_Img/taj5.jpg";
+    document.getElementsByClassName('cimg')[7].src = "./Project_Img/taj6.jpg";
+    document.getElementsByClassName('cimg')[8].src = "./Project_Img/taj7.jpg";
+    document.getElementsByClassName('cimg')[9].src = "./Project_Img/taj8.jpg";
+    document.getElementsByClassName('cimg')[10].src = "./Project_Img/taj9.jpg";
+    document.getElementsByClassName('cimg')[11].src = "./Project_Img/taj10.jpg";
+});
+
+
+
